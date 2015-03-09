c66a39e0c833de18137d2e588a17aa19716030c1
Konosprod <konosprod@free.fr>
2015-03-09 15:25:32 +0100

    Add qBittorrent client handling

--- haruichan-dl.py
+++ haruichan-dl.py
@@ -1,6 +1,8 @@
 from tkinter import Tk
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
+import argparse
 import sys
 
 def copy_to_clipboard(string):
@@ -47,19 +49,46 @@ def get_url(format, team, releases):
 	
 	return urls
 	
- 
-	
-def main():
+def add_to_client(urls):
+	payload = {'urls' : urls}
+	headers = {'Content-Type'  : 'application/x-www-form-urlencoded'}
+	r = requests.post("http://127.0.0.1:8080/command/download/", data=payload, headers=headers)
+	return 0
+		
+def get_urls():
 	(url, format, team) = get_infos()
 	html = get_source(url)
 	releases = get_release(html)
 	
 	urls = get_url(format, team, releases)
 	
-	copy_to_clipboard(urls)
+	#add_to_client(urls)
 	
-	print("Les liens sont dans votre presse-papier")
-	input()
+	#copy_to_clipboard(urls)
+	
+	#print("Les liens sont dans votre presse-papier")
+	
+	return urls
+
+def print_menu():
+	choix = int(input("Que voulez-vous ?\n\t1) Presse papier\n\t2) Ajouter au clien torrent\n: "))
+	while(choix < 1 and choid > 2):
+		choix = int(input("Que voulez-vous ?\n\t1) Presse papier\n\t2) Ajouter au clien torrent\n: "))
+	
+	return choix
+	
+def main():
+	
+	choix = print_menu() 
+	
+	urls = get_urls()
+	
+	if choix == 1:
+		copy_to_clipboard(urls)
+	else:
+		add_to_client(urls)
+	
+	return 0
 	
 if __name__ == "__main__":
 	main()
